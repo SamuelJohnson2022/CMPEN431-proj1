f2bd9978fd3ea22dd3244754f54980198338b63c
SamuelJohnson2022 <samueljohnson2018@gmail.com>
2020-10-28 12:23:42 -0700

    messing with the cache latency

--- YOURCODEHERE.cpp
+++ YOURCODEHERE.cpp
@@ -49,8 +49,8 @@ std::string generateCacheLatencyParams(string halfBackedConfig) {
 
 	std::stringstream latencySettings; // Order is D I U.
 
-    int d1ExponentValue = getdl1size(halfBackedConfig) / 1024; // Exponent value to pass to log base 2.
-    int d1LatencyValue = log2(d1ExponentValue); // Log value to pass for latency settings. 
+    double d1ExponentValue = getdl1size(halfBackedConfig) / 1024; // Exponent value to pass to log base 2.
+    double d1LatencyValue = log2(d1ExponentValue); // Log value to pass for latency settings. 
     int d1AssociateValue = extractConfigPararm(halfBackedConfig, 4); // Get the index value for the associate D1 cache.
 
     if (d1AssociateValue == 1) { // If the index value is 1, add 1 to the latency.
@@ -59,10 +59,10 @@ std::string generateCacheLatencyParams(string halfBackedConfig) {
         d1LatencyValue = d1LatencyValue + 2;
     }
 
-    latencySettings << (d1LatencyValue - 1) << " "; // Pass value into latency settings.
+    latencySettings << ((int)d1LatencyValue - 1) << " "; // Pass value into latency settings.
 
-    int i1ExponentValue = getil1size(halfBackedConfig) / 1024; // Exponent value to pass for il1. 
-    int i1LatencyValue = log2(i1ExponentValue);
+    double i1ExponentValue = getil1size(halfBackedConfig) / 1024; // Exponent value to pass for il1. 
+    double i1LatencyValue = log2(i1ExponentValue);
     int i1AssociativeValue = extractConfigPararm(halfBackedConfig, 6);
 
     if (i1AssociativeValue == 1) {
@@ -71,10 +71,10 @@ std::string generateCacheLatencyParams(string halfBackedConfig) {
         i1LatencyValue = i1LatencyValue + 2;
     }
 
-    latencySettings << (i1LatencyValue - 1) << " ";
+    latencySettings << ((int)i1LatencyValue - 1) << " ";
 	
-    int l2ExponentValue = getl2size(halfBackedConfig) / 1024; // Exponent value to pass for l2. 
-    int l2LatencyValue = log2(l2ExponentValue); 
+    double l2ExponentValue = getl2size(halfBackedConfig) / 1024; // Exponent value to pass for l2. 
+    double l2LatencyValue = log2(l2ExponentValue); 
     int l2AssociativeValue = extractConfigPararm(halfBackedConfig, 9);
 	
 
@@ -88,9 +88,9 @@ std::string generateCacheLatencyParams(string halfBackedConfig) {
         l2LatencyValue = l2LatencyValue + 4;
     }
 
-    latencySettings << (l2LatencyValue - 5);
+    latencySettings << ((int)l2LatencyValue - 5);
 
-	printf("d1 Latency: %d | l1 Latency: %d | l2 Latency: %d", d1LatencyValue, i1LatencyValue, l2LatencyValue);
+	//printf("d1 Latency: %d | l1 Latency: %d | l2 Latency: %d", d1LatencyValue, i1LatencyValue, l2LatencyValue);
 
     return latencySettings.str();
 }
